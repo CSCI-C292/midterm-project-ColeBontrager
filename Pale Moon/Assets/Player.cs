cd1c9ca678443a76c82e93618597a4bb1836a2ec
cobontra <cobontra@iu.edu>
2020-10-14 20:01:42 -0400

    repo setup

---
+++ Pale Moon/Assets/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float _moveSpeed;
+    [SerializeField] float _fallSpeed;
+    [SerializeField] float _jumpForce;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("jump");
+            rb.AddForce(new Vector2(0, _jumpForce));
+        }
+        
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * _moveSpeed * Time.deltaTime, rb.velocity.y);
+        
+        
+    }
+}
